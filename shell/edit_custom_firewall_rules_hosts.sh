cae2d6854ef6d3d30e30b20fa93a309823a16785
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2025-02-06 23:48:12 +0800

    Update ad-block

--- shell/edit_custom_firewall_rules_hosts.sh
+++ shell/edit_custom_firewall_rules_hosts.sh
@@ -10,6 +10,7 @@ INSERT_CONTENT=$(cat << EOF
     MAX_WAIT_TIME=30
     WAIT_INTERVAL=2
     elapsed_time=0
+
     while ! /etc/init.d/openclash status | grep -q "running"; do
         if [ $elapsed_time -ge $MAX_WAIT_TIME ]; then
             LOG_OUT "[广告过滤规则拉取脚本] 未能在 30 秒内检测到 OpenClash 运行状态，脚本已停止运行..."
@@ -19,22 +20,29 @@ INSERT_CONTENT=$(cat << EOF
         sleep $WAIT_INTERVAL
         elapsed_time=$((elapsed_time + WAIT_INTERVAL))
     done
+
     LOG_OUT "[广告过滤规则拉取脚本] 检测到 OpenClash 正在运行，10秒后开始拉取规则..."
     sleep 10
+
     LOG_OUT "[广告过滤规则拉取脚本] 清除已有的 GitHub520 加速规则…"
     sed -i '/# GitHub520 Host Start/,/# GitHub520 Host End/d' /etc/hosts
+
     LOG_OUT "[广告过滤规则拉取脚本] 拉取最新的 GitHub520 加速规则…"
     curl -s "https://raw.hellogithub.com/hosts" >> /etc/hosts 2> /tmp/github520-curl.log
+
     if [ $? -eq 0 ]; then
         LOG_OUT "[广告过滤规则拉取脚本] GitHub520 加速规则拉取成功！"
     else
         LOG_OUT "[广告过滤规则拉取脚本] GitHub520 加速规则拉取失败，查看 /tmp/github520-curl.log 获取详细信息。"
     fi
+
     sed -i '/^$/d' /etc/hosts
     sed -i '/!/d' /etc/hosts
+
     LOG_OUT "[广告过滤规则拉取脚本] 清理 DNS 缓存…"
     /etc/init.d/dnsmasq reload
     LOG_OUT "[广告过滤规则拉取脚本] 脚本运行完毕！"
+
 ) &
 # ==============广告过滤规则拉取脚本结束==============
 EOF
