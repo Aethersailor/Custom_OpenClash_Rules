66c60253383405b3bbf7ecd9edcd2848a01e2a78
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2025-02-08 00:31:53 +0800

    Update ad-block

--- shell/edit_custom_firewall_rules_anti-ad+github520.sh
+++ shell/edit_custom_firewall_rules_anti-ad+github520.sh
@@ -34,6 +34,7 @@ INSERT_CONTENT=$(cat << EOF
     sed -i '/# AWAvenue-Ads-Rule Start/,/# AWAvenue-Ads-Rule End/d' /etc/hosts
 
     LOG_OUT "[广告过滤规则拉取脚本] 拉取最新的 anti-AD 广告过滤规则，规则体积较大，请耐心等候…"
+    mkdir -p /tmp/dnsmasq.cfg01411c.d
     curl -s  "https://github.boki.moe/https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/adblock-for-dnsmasq.conf" -o /tmp/dnsmasq.cfg01411c.d/anti-ad-for-dnsmasq.conf 2> /tmp/anti-ad-curl.log
 
     if [ $? -eq 0 ]; then
