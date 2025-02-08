91bfc9ca8fc893d60860b73f91775ae9ffcb4a1d
Aethersailor <ramana855patel@gmail.com>
2025-02-08 17:02:08 +0800

    Update edit_custom_firewall_rules.sh

--- shell/edit_custom_firewall_rules.sh
+++ shell/edit_custom_firewall_rules.sh
@@ -114,7 +114,7 @@ if [ "$adv_choice" = "y" ] || [ "$github_choice" = "y" ]; then
 
     LOG_OUT \"[广告过滤规则拉取脚本] 拉取最新的 anti-AD 广告过滤规则，规则体积较大，请耐心等候...\"
     mkdir -p /tmp/dnsmasq.cfg01411c.d
-    curl -s \"https://gh-proxy.com/https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/adblock-for-dnsmasq.conf\" -o /tmp/dnsmasq.cfg01411c.d/anti-ad-for-dnsmasq.conf 2> /tmp/anti-ad-curl.log
+    curl -sSL \"https://github.boki.moe/https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/adblock-for-dnsmasq.conf\" -o /tmp/dnsmasq.cfg01411c.d/anti-ad-for-dnsmasq.conf 2> /tmp/anti-ad-curl.log
 
     if [ \$? -eq 0 ]; then
         LOG_OUT \"[广告过滤规则拉取脚本] anti-AD 规则拉取成功!\"
@@ -132,7 +132,7 @@ if [ "$adv_choice" = "y" ] || [ "$github_choice" = "y" ]; then
 
     LOG_OUT \"[广告过滤规则拉取脚本] 拉取最新的 adblockfilters 广告过滤规则，规则体积较大，请耐心等候...\"
     mkdir -p /tmp/dnsmasq.cfg01411c.d
-    curl -s \"https://gh-proxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt\" -o /tmp/dnsmasq.cfg01411c.d/adblockfilters-for-dnsmasq.conf 2> /tmp/adblockfilters-curl.log
+    curl -sSL \"https://github.boki.moe/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt\" -o /tmp/dnsmasq.cfg01411c.d/adblockfilters-for-dnsmasq.conf 2> /tmp/adblockfilters-curl.log
 
     if [ \$? -eq 0 ]; then
         LOG_OUT \"[广告过滤规则拉取脚本] adblockfilters 规则拉取成功!\"
@@ -161,7 +161,7 @@ if [ "$adv_choice" = "y" ] || [ "$github_choice" = "y" ]; then
     sed -i '/# GitHub520 Host Start/,/# GitHub520 Host End/d' /etc/hosts
 
     LOG_OUT \"[广告过滤规则拉取脚本] 拉取最新的 GitHub520 加速规则...\"
-    curl -s \"https://raw.hellogithub.com/hosts\" >> /etc/hosts 2> /tmp/github520-curl.log
+    curl -sSL \"https://raw.hellogithub.com/hosts\" >> /etc/hosts 2> /tmp/github520-curl.log
 
     if [ \$? -eq 0 ]; then
         LOG_OUT \"[广告过滤规则拉取脚本] GitHub520 加速规则拉取成功!\"
