31d6494f4daad05ce9798b89e2c95de764715f11
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2025-02-18 00:32:12 +0800

    Update .sh files

--- shell/edit_custom_firewall_rules_adblockfilters-modified+github520.sh
+++ shell/edit_custom_firewall_rules_adblockfilters-modified+github520.sh
@@ -36,7 +36,7 @@ INSERT_CONTENT=$(cat << EOF
 
     LOG_OUT "[广告过滤规则拉取脚本] 拉取最新的 adblockfilters-modified 广告过滤规则，规则体积较大，请耐心等候…"
     mkdir -p /tmp/dnsmasq.cfg01411c.d
-    curl -sSL --retry 5 --retry-delay 1 "https://github.boki.moe/https://raw.githubusercontent.com/Aethersailor/adblockfilters-modified/refs/heads/main/rules/adblockdnsmasq.txt" -o /tmp/dnsmasq.cfg01411c.d/adblockfilters-modified-for-dnsmasq.conf 2> /tmp/adblockfilters-modified-curl.log
+    curl -sSL -4 --retry 5 --retry-delay 1 "https://github.boki.moe/https://raw.githubusercontent.com/Aethersailor/adblockfilters-modified/refs/heads/main/rules/adblockdnsmasq.txt" -o /tmp/dnsmasq.cfg01411c.d/adblockfilters-modified-for-dnsmasq.conf 2> /tmp/adblockfilters-modified-curl.log
 
     if [ $? -eq 0 ]; then
         LOG_OUT "[广告过滤规则拉取脚本] adblockfilters-modified 规则拉取成功！"
@@ -48,7 +48,7 @@ INSERT_CONTENT=$(cat << EOF
     sed -i '/# GitHub520 Host Start/,/# GitHub520 Host End/d' /etc/hosts
 
     LOG_OUT "[广告过滤规则拉取脚本] 拉取最新的 GitHub520 加速规则…"
-    curl -sSL --retry 5 --retry-delay 1 "https://raw.hellogithub.com/hosts" >> /etc/hosts 2> /tmp/github520-curl.log
+    curl -sSL -4 --retry 5 --retry-delay 1 "https://raw.hellogithub.com/hosts" >> /etc/hosts 2> /tmp/github520-curl.log
 
     if [ $? -eq 0 ]; then
         LOG_OUT "[广告过滤规则拉取脚本] GitHub520 加速规则拉取成功！"
