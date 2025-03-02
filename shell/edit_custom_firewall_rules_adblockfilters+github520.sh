88afe8d7709cc2e08a4c6bdd3f9cd774c8d4060e
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2025-03-03 00:11:23 +0800

    Update .sh files

--- shell/edit_custom_firewall_rules_adblockfilters+github520.sh
+++ shell/edit_custom_firewall_rules_adblockfilters+github520.sh
@@ -35,8 +35,8 @@ INSERT_CONTENT=$(cat << EOF
 
 
     LOG_OUT "[广告过滤规则拉取脚本] 拉取最新的 adblockfilters 广告过滤规则，规则体积较大，请耐心等候…"
-    mkdir -p /tmp/dnsmasq.cfg01411c.d
-    curl -sSL -4 --retry 5 --retry-delay 1 "https://github.boki.moe/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt" -o /tmp/dnsmasq.cfg01411c.d/adblockfilters-for-dnsmasq.conf 2> /tmp/adblockfilters-curl.log
+    mkdir -p /tmp/dnsmasq.d
+    curl -sSL -4 --retry 5 --retry-delay 1 "https://github.boki.moe/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt" -o /tmp/dnsmasq.d/adblockfilters-for-dnsmasq.conf 2> /tmp/adblockfilters-curl.log
 
     if [ $? -eq 0 ]; then
         LOG_OUT "[广告过滤规则拉取脚本] adblockfilters 规则拉取成功！"
