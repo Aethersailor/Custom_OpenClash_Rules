d97d73873fe264bba5c28fef1a9f59fe52a79888
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2025-05-02 10:43:20 +0800

    Update install_openclash_dev_opkg.sh

--- shell/install_openclash_dev_opkg.sh
+++ shell/install_openclash_dev_opkg.sh
@@ -65,3 +65,61 @@ fi
 
 # 完成更新提示
 echo "Meta 内核更新完成！"
+
+# 开始更新 GeoIP 数据库
+echo "开始更新 GeoIP Dat 数据库..."
+/usr/share/openclash/openclash_geoip.sh
+if [ $? -ne 0 ]; then
+  echo "GeoIP Dat 数据库更新失败，请检查日志。"
+  exit 1
+fi
+echo "GeoIP Dat 数据库更新完成！"
+
+# 开始更新 IP 数据库
+echo "开始更新 GeoIP MMDB 数据库..."
+/usr/share/openclash/openclash_ipdb.sh
+if [ $? -ne 0 ]; then
+  echo "GeoIP MMDB 数据库更新失败，请检查日志。"
+  exit 1
+fi
+echo "GeoIP MMDB 数据库更新完成！"
+
+# 开始更新 GeoSite 数据库
+echo "开始更新 GeoSite 数据库..."
+/usr/share/openclash/openclash_geosite.sh
+if [ $? -ne 0 ]; then
+  echo "GeoSite 数据库更新失败，请检查日志。"
+  exit 1
+fi
+echo "GeoSite 数据库更新完成！"
+
+# 开始更新 GeoASN 数据库
+echo "开始更新 GeoASN 数据库..."
+/usr/share/openclash/openclash_geoasn.sh
+if [ $? -ne 0 ]; then
+  echo "GeoASN 数据库更新失败，请检查日志。"
+  exit 1
+fi
+echo "GeoASN 数据库更新完成！"
+
+# 开始更新大陆白名单
+echo "开始更新大陆白名单..."
+/usr/share/openclash/openclash_chnroute.sh
+if [ $? -ne 0 ]; then
+  echo "大陆白名单更新失败，请检查日志。"
+  exit 1
+fi
+echo "大陆白名单更新完成！"
+
+# 开始更新订阅
+echo "正在更新订阅..."
+/usr/share/openclash/openclash.sh
+if [ $? -ne 0 ]; then
+  echo "订阅更新失败，请检查日志。"
+  exit 1
+fi
+echo "订阅更新完成！"
+
+sleep 3
+echo "启动 OpenClash ..."
+/etc/init.d/openclash restart
